e670d6813ddfd11c93d6eb0546bcbd8d486b9d0f
Joseph Basile <jbasile2@ivytech.edu>
2024-06-23 10:55:08 -0400

    Add files via upload

--- M03 Lab Case Study_JosephBasile.py
+++ M03 Lab Case Study_JosephBasile.py
@@ -1,30 +1,37 @@
+# The parent class that defines vehicle type, in this case it'll be car
 class Vehicle():
     def __init__(self, type):
         self.type = 'car'
-    
+
+    # This helps make the end result look cleaner when it prints it
     def user_vehicle_type(self):
         print("Vehicle Type:", self.type)
     
-
+# The Automobile class which inherits from the Vehicle class type
 class Automobile(Vehicle):
     def __init__(self, type):
+        # inheriting type from Vehicle class
         super().__init__(type)
+        # Setting the different parts to this for the user to enter their input
         self.year = input("Please enter the year your car was made.")
         self.make = input("Please enter the company that made your vehicle.")
         self.model = input("Please enter your vehicles model.")
         self.doors = input("Please enter whether your vehicle has 2 or 4 doors.")
+        # Since the assignment wanted 2 or 4 doors I made a while loop to check if it's 2 or 4
         while self.doors != 2 or 4:
             print("Please enter either 2 or 4")
             self.doors = input("Please enter whether your vehicle has 2 or 4 doors.")
             if self.doors == 2 or 4:
                 break
         self.roof = input("Please enter either solid or sun for the type of roof your vehicle has.")
+        # Similar to the amount of doors the assignment wanted to check if the vehicle had a solid or sun roof so I made this while loop
         while self.roof != 'solid' or 'sun':
             print("Please enter either solid or sun")
             self.roof = input("Please enter either solid or sun for the type of roof your vehicle has.")
             if self.roof == 'solid' or 'sun':
                 break
-    
+
+    # defining various functions within the class to print the users input
     def user_car_year(self):
         print("Year:", self.year)
     
@@ -40,6 +47,8 @@ class Automobile(Vehicle):
     def user_car_roof(self):
         print("Type of roof:", self.roof, 'roof')
 
+# Calls on Automobile, which then asks the user for year, make, model, doors, and roof. Type or preset as per assignment instructions
+# Prints the results in a nice clean way
 user_year = Automobile('car')
 user_year.user_vehicle_type()
 user_year.user_car_year()
